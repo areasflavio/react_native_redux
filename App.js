6befe7b825428dca0495dc55cc7679f1764701c7
Flávio Arêas <vflavio9@gmail.com>
2022-10-04 16:31:27 -0300

    chore: set up redux

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { configureStore } from '@reduxjs/toolkit';
 import { StatusBar } from 'expo-status-bar';
+import { Provider } from 'react-redux';
 
+import { filmesReducer } from './src/reducers/filmesReducer';
 import { Details } from './src/screens/Details';
 import { Home } from './src/screens/Home';
 
 const Stack = createNativeStackNavigator();
 
+const store = configureStore({
+  reducer: {
+    filmes: filmesReducer,
+  },
+});
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <StatusBar style="auto" />
+    <Provider store={store}>
+      <NavigationContainer>
+        <StatusBar style="auto" />
 
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Details" component={Details} />
-      </Stack.Navigator>
-    </NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Details" component={Details} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
